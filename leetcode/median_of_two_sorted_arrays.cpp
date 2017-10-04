54f12979ec004361f27d89a0eae9675221116bbf
Alex Wang <alexander.gj.wang@gmail.com>
2017-10-03 23:55:32 -0400

    moved files

---
+++ leetcode/median_of_two_sorted_arrays.cpp
@@ -0,0 +1,9 @@
+class Solution
+{
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+        int m = nums1.size(), n = nums2.size();
+        // TODO:
+    }
+};
