0e7a1d3d4f23ad2a23a5b919c78cb04bf2e74f7b
Alex Wang <alexander.gj.wang@gmail.com>
2017-12-28 16:17:53 -0500

    added files

---
+++ leetcode/stock.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = 0, buy_candidate = 0, max = 0;
+
+        for (int i = 1; i < prices.size(); i++)
+        {
+            if (prices[i] - prices[buy_candidate] > max)
+            {
+                max = prices[i] - prices[buy_candidate];
+                buy = buy_candidate;
+            }
+
+            if (prices[i] < prices[buy_candidate])
+                buy_candidate = i;
+        }
+
+        return max > 0 ? max : 0;
+    }
+};
+
