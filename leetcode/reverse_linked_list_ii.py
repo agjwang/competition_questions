e34e68eb3cbdb8157c58e899f561800aaf766a74
Alex Wang <alexander.gj.wang@gmail.com>
2018-03-25 00:39:15 -0400

    added reverse_linked_list_ii.py

---
+++ leetcode/reverse_linked_list_ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverseBetween(self, head, m, n):
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        pre = dummyNode
+
+        for i in range(m - 1):
+            pre = pre.next
+        
+        # reverse the [m, n] nodes
+        reverse = None
+        cur = pre.next
+        for i in range(n - m + 1):
+            next = cur.next
+            cur.next = reverse
+            reverse = cur
+            cur = next
+            print(dummyNode.next.val)
+
+        pre.next.next = cur
+        pre.next = reverse
+        
+        
+
+        return dummyNode.next
